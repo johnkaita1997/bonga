95126cb346cb72e1f3ad5656776c7a3bae078015
kaita <johnkaitadocuments@gmail.com>
2023-06-12 12:52:53 +0300

    j

--- mobile/api/urls.py
+++ mobile/api/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('create', MobileCreateView.as_view(), name="mobile-create"),
     path('list', MobileListView.as_view(), name="mobile-list"),
     path('<int:pk>', MobileDetailView.as_view(), name="mobile-detail"),
-    # path('id/<int:pk>', MobileIdDetailView.as_view(), name="myschoolmy-detail"),
     path('id/<int:pk>', MobileIdDetailView.as_view(), name="myschoolmy-detail"),
 ]
