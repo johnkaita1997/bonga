a9cfdb05db4ceea45bcb520f58a094df3b4b316f
kaita <johnkaitadocuments@gmail.com>
2023-06-23 12:13:44 +0300

    zh

--- bonga/settings.py
+++ bonga/settings.py
@@ -86,12 +86,26 @@ WSGI_APPLICATION = 'bonga.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bongadb',
+        'USER': 'bonga',
+        'PASSWORD': 'Login2aws',
+        'HOST': 'database-1.c8bqhdkr5dbp.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
